bce21af8e59abb91893be82b5c9e46e6c39fc31b
Ramon Santos <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2026-06-01 12:34:03 -0400

    trabajando menu

--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {  NgxPrintModule } from 'ngx-print';
 import { MatButtonModule, MatAnchor } from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar'
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-menu',
-  imports: [NgxPrintModule ],
+  imports: [NgxPrintModule, MatToolbarModule, MatButtonModule, RouterLink],
   templateUrl: './menu.html',
 })
 export class Menu { }
